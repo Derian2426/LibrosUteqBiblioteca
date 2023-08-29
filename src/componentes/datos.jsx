5091f0e326f63886e2efa4732f01368c4dd27e3c
Derian2426 <victor.chun2017@uteq.edu.ec>
2023-08-29 12:19:13 -0500

    buscador

--- src/componentes/datos.jsx
+++ src/componentes/datos.jsx
@@ -10,14 +10,22 @@ import { LibroContext } from "../context/LibrosContext";
 
 export function Tabladatos() {
   const [currentPage, setCurrentPage] = useState(0);
+  const [busquedaLibro, setBusquedaLibro] = useState("");
   const itemsPerPage = 3;
   const handleButtonClick = async (idLibro) => {
     window.location.href = `/audiolibro/${idLibro}`;
   };
-  const { listaLibro } = useContext(LibroContext);
+  const { listaLibro,postDataJson } = useContext(LibroContext);
 
   const handlePageChange = ({ selected }) => {
     setCurrentPage(selected);
+  };
+  const handleBusqueda = () => {
+    const jsonBusqueda = {
+      cadenaBusqueda: busquedaLibro,
+      fechaPublicacion: "",
+    };
+    postDataJson(jsonBusqueda,"/libro");
   };
 
   const paginatedData = listaLibro.slice(
@@ -27,6 +35,41 @@ export function Tabladatos() {
 
   return (
     <div className="Mycontainer-div-table" style={{ maxWidth: "1320px" }}>
+      <form className="row g-3 needs-validation" noValidate>
+        <div className="col-md-11">
+          <label htmlFor="Label-Autor" className="form-label">
+            Nombre Libro:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="Buscar-libro"
+            placeholder="Buscar por nombre del libro"
+            value={busquedaLibro}
+            onChange={(event) => setBusquedaLibro(event.target.value)}
+            required
+          />
+          <div className="valid-feedback"></div>
+        </div>
+        <div className="col-md-1">
+          <label
+            htmlFor="boton-buscar-libros"
+            className="form-label"
+            style={{ opacity: 0 }}
+          >
+            Boton
+          </label>
+          <button
+            className="btn btn-success"
+            aria-label="Boton para buscar los libros"
+            type="button"
+            onClick={handleBusqueda}
+          >
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
+      </form>
+
       <Table aria-label="Lista de todos los libros">
         <thead>
           <tr>
@@ -41,9 +84,9 @@ export function Tabladatos() {
           {paginatedData.map((dato, index) => (
             <tr key={index}>
               <th scope="row">{currentPage * itemsPerPage + index + 1}</th>
-              <td  style={{textAlign:"left" }}>{dato.nombreLibro}</td>
-              <td  style={{textAlign:"center" }}>{dato.fechaPublicacion}</td>
-              <td style={{textAlign:"center" }}>{dato.lenguaje}</td>
+              <td style={{ textAlign: "left" }}>{dato.nombreLibro}</td>
+              <td style={{ textAlign: "center" }}>{dato.fechaPublicacion}</td>
+              <td style={{ textAlign: "center" }}>{dato.lenguaje}</td>
               <td>
                 <button
                   className="audio-button"
@@ -56,9 +99,9 @@ export function Tabladatos() {
           ))}
         </tbody>
       </Table>
-      
+
       <ReactPaginate
-        previousLabel={"<"} 
+        previousLabel={"<"}
         nextLabel={">"}
         breakLabel={"..."}
         breakClassName={"break-me"}
--- src/context/LibrosContext.jsx
+++ src/context/LibrosContext.jsx
@@ -1,24 +1,32 @@
 import { createContext, useEffect, useState } from "react";
 import config from "../configuracion";
-import { obtenerDatos } from "../peticionesHttp";
+import { obtenerDatos, postData } from "../peticionesHttp";
 import { LoadingDialog } from "../LoadingDialog";
 
 export const LibroContext = createContext();
 export function LibroContextProvider(props) {
-  const libroUrl = config.libroUrl + "/libro";
+  const libroUrl = config.libroUrl;
   const [listaLibro, setListaLibro] = useState([]);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
-    obtenerDatos(libroUrl)
+    obtenerDatos(libroUrl + "/libro")
       .then(async (data) => {
         setListaLibro(data);
         setLoading(false);
       })
       .catch((error) => console.error(error));
   }, []);
+  const postDataJson = async (jsonData, url) => {
+    try {
+      const peticionPost = await postData(libroUrl + url, jsonData);
+      setListaLibro(peticionPost);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <LibroContext.Provider value={{ listaLibro }}>
+    <LibroContext.Provider value={{ listaLibro,postDataJson }}>
       <LoadingDialog loading={loading} />
       {props.children}
     </LibroContext.Provider>
