619a9b4c4553373730eaf7b34c3bde3cb4d7abcd
Derian2426 <victor.chun2017@uteq.edu.ec>
2023-09-01 18:51:48 -0500

    Update peticionesHttp.jsx

--- src/peticionesHttp.jsx
+++ src/peticionesHttp.jsx
@@ -1,14 +1,39 @@
 import axios from "axios";
 
-export const obtenerDatos = (url) => {
-  return axios.get(url).then((response) => response.data);
+function obtenerToken() {
+  try {
+    const loggerUser = localStorage.getItem("loggerUser");
+    if (loggerUser) {
+      const jsonObject = JSON.parse(loggerUser);
+      if (jsonObject.token) {
+        return jsonObject.token;
+      }
+    }
+    throw new Error("Token no encontrado en el Local Storage");
+  } catch (error) {
+    console.error("Error al obtener el token:", error);
+    return "";
+  }
+}
+
+export const obtenerDatos = async (url) => {
+  const token = obtenerToken();
+  return axios
+    .get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => response.data);
 };
 
 export const postData = async (url, jsonData) => {
+  const token = obtenerToken();
   try {
     const response = await axios.post(url, jsonData, {
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
     });
     if (response.status !== 200) {
@@ -21,10 +46,12 @@ export const postData = async (url, jsonData) => {
 };
 
 export const enviarPeticionConEncabezadoJSON = async (url, jsonData) => {
+  const token = obtenerToken();
   const response = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(jsonData),
   });
